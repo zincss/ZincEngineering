d3f33db68e3808a17a26633433e4b3060adc48b0
zincss <zacfmckinley@gmail.com>
2025-12-18 13:42:32 +1100

    search

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,21 +7,11 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Advanced Tactical & Athletic Telemetry',
     start_url: '/',
     display: 'standalone',
-    background_color: '#fafafa',
-    theme_color: '#fafafa',
+    background_color: '#09090b', // Changed to zinc-950 to match your dark theme
+    theme_color: '#09090b', // Changed to zinc-950
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
-      },
-      {
-        src: '/icon-192.png', // You should add these images to your /public folder
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512.png', // You should add these images to your /public folder
+        src: '/icon', // This points to the file we just created
         sizes: '512x512',
         type: 'image/png',
       },
