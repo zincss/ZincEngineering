747cfda3eec6b501c7f97d771e1382d5c2fa6470
zac <zacfmckinley@gmail.com>
2025-12-18 20:42:59 +1100

    market refine

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,3 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
-
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
-}
-
 export const config = {
   matcher: [
     /*
@@ -12,8 +5,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public files (images, etc)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * - PWA files - .js, .webmanifest, .json
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|js|webmanifest|json)$).*)',
   ],
 }
